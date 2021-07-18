deb34a82a189367ed4b077da70f7031f2e891dd3
tomyusuke <tomyusuke.ksu@gmail.com>
2021-07-18 14:40:09 +0900

    #2

--- react-ts-app/src/index.tsx
+++ react-ts-app/src/index.tsx
@@ -2,62 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user: Obj_user){
-  return user.firstName + " " + user.lastName;
+function formatName(user: User){
+  return `${user.firstName} ${user.lastName}`;
 }
 
-interface Obj_user {
+interface User {
   firstName: string;
   lastName: string;
   avatarUrl: string;
 }
 
-const user: Obj_user = {
+const user: User = {
   firstName: "toma",
   lastName: "yu",
   avatarUrl: "https://placehold.jp/150x150.png"
 }
 
-function getGreeting(user: Obj_user) {
+function getGreeting(user: User) {
   if(user){
     return <h1>hello, {formatName(user)}!</h1>
   }
   return <h1>hello, stranger!</h1>
 }
 
-const element_div = (
-  <div>
-    <div id="1"></div>
-    <div id="2"></div>
-    <div id="3"></div>
-  </div>
-);
-const element_img = <img src={user.avatarUrl} alt="test" />
-const element = getGreeting(user);
-const element_createElement = React.createElement(
+const elementGreeting = getGreeting(user);
+const elementImg = <img src={user.avatarUrl} alt="test" />
+const elementCreateElement = React.createElement(
   "h1",
   {className: "greeting"},
   "こんにちは"
 )
 
-ReactDOM.render(
-  element_div,
-  document.getElementById("root")
-)
+const element = (
+  <div>
+    <div id="1">{elementGreeting}</div>
+    <div id="2">{elementImg}</div>
+    <div id="3">{elementCreateElement}</div>
+  </div>
+);
 
 ReactDOM.render(
   element,
-  document.getElementById("1")
-);
-
-ReactDOM.render(
-  element_img,
-  document.getElementById("2")
-);
-
-ReactDOM.render(
-  element_createElement,
-  document.getElementById("3")
+  document.getElementById("root")
 )
 
 // If you want to start measuring performance in your app, pass a function
