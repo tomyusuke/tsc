c32a79d233342d9ed4f94b72f3a2e33b9846ec4a
tomyusuke <tomyusuke.ksu@gmail.com>
2021-07-19 20:59:46 +0900

    move

--- react-ts-app/src/component/Clock.tsx
+++ react-ts-app/src/component/Clock.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
-class Clock extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {date: new Date()};
-	}
 
-	render(){
-		return (
-			<div>
-				<h1>hello, world</h1>
-				<h2>It is {this.state.date.toLocalTimeString()}.</h2>
-			</div>
-		)
-	}
+type Props = {}
+type State = {date : Date}
+class Clock extends React.Component<Props, State> {
+	
+  constructor(props: Props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 export default Clock;
