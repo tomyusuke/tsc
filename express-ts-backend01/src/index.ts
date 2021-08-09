4c6a82fad91ccf341a9374b6534cc1ddddc96305
tomyusuke <tomyusuke.ksu@gmail.com>
2021-08-09 16:17:22 +0900

    delete insert sampledata

--- express-ts-backend01/src/index.ts
+++ express-ts-backend01/src/index.ts
@@ -1,10 +1,6 @@
-import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { Request, Response } from 'express';
-import { User } from './entity/User';
-import { UserController } from './controller/UserController';
 import { router as userRouter } from './router/userRouter';
 
 createConnection()
@@ -22,22 +18,6 @@ createConnection()
 
     // start express server
     app.listen(3000);
-
-    // insert new users for test
-    await connection.manager.save(
-      connection.manager.create(User, {
-        firstName: 'Timber',
-        lastName: 'Saw',
-        age: 27,
-      })
-    );
-    await connection.manager.save(
-      connection.manager.create(User, {
-        firstName: 'Phantom',
-        lastName: 'Assassin',
-        age: 24,
-      })
-    );
 
     console.log(
       'Express server has started on port 3000. Open http://localhost:3000/user/ to see results'
